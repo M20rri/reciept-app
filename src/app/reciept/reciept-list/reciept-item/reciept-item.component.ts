d3103301c26b58614c6d561cf67a83aa2df8f392
M2ri <SergiGullam@gmail.com>
2024-06-29 04:06:47 +0300

    apply reciept-item

--- src/app/reciept/reciept-list/reciept-item/reciept-item.component.ts
+++ src/app/reciept/reciept-list/reciept-item/reciept-item.component.ts
@@ -1,17 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IReciept } from '../../reciept.model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reciept-item',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './reciept-item.component.html',
   styleUrl: './reciept-item.component.css',
 })
-export class RecieptItemComponent {
-  // @Input({ alias: 'reciept-list', required: true }) reciepts: Array<IReciept>;
-  @Input({ alias: 'message', required: true }) message: string;
-  constructor() {
-    console.log('message => RecieptItemComponent', this.message);
+export class RecieptItemComponent implements OnInit {
+  @Input({ alias: 'reciept-item', required: true }) reciept: IReciept;
+  constructor() {}
+  ngOnInit(): void {
+    console.log('reciept', this.reciept);
   }
 }
--- src/app/reciept/reciept-list/reciept-list.component.ts
+++ src/app/reciept/reciept-list/reciept-list.component.ts
@@ -1,44 +1,46 @@
 import { Component, Input } from '@angular/core';
 import { RecieptItemComponent } from './reciept-item/reciept-item.component';
 import { IReciept } from '../reciept.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reciept-list',
   standalone: true,
   templateUrl: './reciept-list.component.html',
   styleUrl: './reciept-list.component.css',
-  imports: [RecieptItemComponent],
+  imports: [RecieptItemComponent, FormsModule, CommonModule],
 })
 export class RecieptListComponent {
   public reciepts: Array<IReciept> = [
     {
       id: 1,
-      name: 'Sample Item 1',
-      description: 'This is the description for Sample Item 1.',
+      name: 'Reciept 1',
+      description: 'This is the description for Reciept 1.',
       imageUrl: 'https://fakeimg.pl/300x200/?text=Sample1',
     },
     {
       id: 2,
-      name: 'Sample Item 2',
-      description: 'This is the description for Sample Item 2.',
+      name: 'Reciept 2',
+      description: 'This is the description for Reciept 2.',
       imageUrl: 'https://fakeimg.pl/300x200/?text=Sample2',
     },
     {
       id: 3,
-      name: 'Sample Item 3',
-      description: 'This is the description for Sample Item 3.',
+      name: 'Reciept 3',
+      description: 'This is the description for Reciept 3.',
       imageUrl: 'https://fakeimg.pl/300x200/?text=Sample3',
     },
     {
       id: 4,
-      name: 'Sample Item 4',
-      description: 'This is the description for Sample Item 4.',
+      name: 'Reciept 4',
+      description: 'This is the description for Reciept 4.',
       imageUrl: 'https://fakeimg.pl/300x200/?text=Sample4',
     },
     {
       id: 5,
-      name: 'Sample Item 5',
-      description: 'This is the description for Sample Item 5.',
+      name: 'Reciept 5',
+      description: 'This is the description for Reciept 5.',
       imageUrl: 'https://fakeimg.pl/300x200/?text=Sample5',
     },
   ];
